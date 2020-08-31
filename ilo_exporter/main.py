832249583647bec5ac0141eed70fc3139883fb5e
JackWindows <me@jackfan.com>
2020-08-31 02:26:08 +0000

    Switch from python-hpilo to rest client
    1. Use the legacy rest client because redfish client doesn't have fan
    speed data.
    2. The rest client can keep session so there won't be tons of
    login/logout logs in iLO event log.

--- ilo_exporter/main.py
+++ ilo_exporter/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import collections
-import hpilo
 import os
+import redfish
 import waitress
 from flask import Flask
 from prometheus_client import make_wsgi_app
@@ -10,68 +10,79 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 
 class ILOCollector(object):
-    def __init__(self, hostname: str, port: int = 443, user: str = 'admin', password: str = 'password',
-                 protocol: str = 'HTTP') -> None:
-        if protocol == 'HTTP':
-            protocol = hpilo.ILO_HTTP
-        elif protocol == 'LOCAL':
-            protocol = hpilo.ILO_LOCAL
-        else:
-            raise RuntimeError(f'unsupported protocol {protocol}')
-        self.ilo = hpilo.Ilo(hostname=hostname, port=port, login=user, password=password, protocol=protocol)
-        self.product_name = self.ilo.get_product_name()
-        self.hostname = hostname
+    def __init__(self, hostname: str, port: int = 443, user: str = 'admin', password: str = 'password') -> None:
+        self.ilo = redfish.LegacyRestClient(base_url=hostname, username=user, password=password)
+        self.ilo.login()
+
+        system = self.ilo.get('/redfish/v1/Systems/1/').obj
+        self.label_names = ('hostname', 'product_name', 'sn')
+        self.label_values = (hostname, system.Model, system.SerialNumber.strip())
 
     def collect(self):
-        embedded_health = self.ilo.get_embedded_health()
+        embedded_media = self.ilo.get('/redfish/v1/Managers/1/EmbeddedMedia/').obj
+        smart_storage = self.ilo.get('/redfish/v1/Systems/1/SmartStorage/').obj
+        thermal = self.ilo.get('/redfish/v1/Chassis/1/Thermal/').obj
+        power = self.ilo.get('/redfish/v1/Chassis/1/Power/').obj
 
-        label_names = ('hostname', 'product_name')
-        label_values = (self.hostname, self.product_name)
+        g = GaugeMetricFamily('hpilo_health',
+                              'iLO health status, -1: Unknown, 0: OK, 1: Degraded, 2: Failed.',
+                              labels=self.label_names + ('component',))
 
-        if embedded_health['health_at_a_glance'] is not None:
-            g = GaugeMetricFamily('hpilo_health_at_a_glance',
-                                  'iLO health at a glance status, 0: Unknown, 1: OK, 2: Degraded, 3: Failed.',
-                                  labels=label_names + ('component',))
-            for key, value in embedded_health['health_at_a_glance'].items():
-                status = value['status'].lower()
-                metric_value = -1
-                if status == 'ok':
-                    metric_value = 0
-                elif status == 'degraded':
-                    metric_value = 1
-                elif status == 'failed':
-                    metric_value = 2
-                g.add_metric(label_values + (key,), metric_value)
+        def status_to_code(status: str) -> int:
+            status = status.lower()
+            ret = -1
+            if status == 'ok':
+                ret = 0
+            elif status == 'degraded':
+                ret = 1
+            elif status == 'failed':
+                ret = 2
+            return ret
+
+        g.add_metric(self.label_values + ('embedded_media',), status_to_code(embedded_media.Controller.Status.Health))
+        g.add_metric(self.label_values + ('smart_storage',), status_to_code(smart_storage.Status.Health))
+        for fan in thermal.Fans:
+            g.add_metric(self.label_values + (fan.FanName,), status_to_code(fan.Status.Health))
+        yield g
+
+        g = GaugeMetricFamily('hpilo_fan_speed', 'Fan speed in percentage.',
+                              labels=self.label_names + ('fan',), unit='percentage')
+        for fan in thermal.Fans:
+            g.add_metric(self.label_values + (fan.FanName,), fan.CurrentReading)
+        yield g
+
+        sensors_by_unit = collections.defaultdict(list)
+        for sensor in thermal.Temperatures:
+            if sensor.Status.State.lower() != 'enabled':
+                continue
+            reading = sensor.CurrentReading
+            unit = sensor.Units
+            sensors_by_unit[unit].append((sensor.Name, reading))
+        for unit in sensors_by_unit:
+            g = GaugeMetricFamily('hpilo_temperature', 'Temperature sensors reading.',
+                                  labels=self.label_names + ('sensor',), unit=unit.lower())
+            for sensor_name, sensor_reading in sensors_by_unit[unit]:
+                g.add_metric(self.label_values + (sensor_name,), sensor_reading)
             yield g
 
-        if embedded_health['fans'] is not None:
-            g = GaugeMetricFamily('hpilo_fan_speed', 'Fan speed in percentage.',
-                                  labels=label_names + ('fan',), unit='percentage')
-            for fan in embedded_health['fans'].values():
-                metric_label = label_values + (fan['label'],)
-                metric_value = fan['speed'][0]
-                g.add_metric(metric_label, metric_value)
-            yield g
+        g = GaugeMetricFamily('hpilo_power_current', 'Current power consumption in Watts.', labels=self.label_names,
+                              unit='watts')
+        g.add_metric(self.label_values, power.PowerConsumedWatts)
+        yield g
 
-        if embedded_health['temperature'] is not None:
-            sensors_by_unit = collections.defaultdict(list)
-            for sensor in embedded_health['temperature'].values():
-                if sensor['currentreading'] == 'N/A':
-                    continue
-                reading, unit = sensor['currentreading']
-                sensors_by_unit[unit].append((sensor['label'], reading))
-            for unit in sensors_by_unit:
-                g = GaugeMetricFamily('hpilo_temperature', 'Temperature sensors reading.',
-                                      labels=label_names + ('sensor',), unit=unit.lower())
-                for sensor_name, sensor_reading in sensors_by_unit[unit]:
-                    g.add_metric(label_values + (sensor_name,), sensor_reading)
-                yield g
-
-        if embedded_health['power_supply_summary'] is not None:
-            g = GaugeMetricFamily('hpilo_power_reading', 'Power reading in Watts.', labels=label_names, unit='watts')
-            gauge_value = int(embedded_health['power_supply_summary']['present_power_reading'].split(' ')[0])
-            g.add_metric(label_values, gauge_value)
-            yield g
+        label_values = self.label_values + (str(power.PowerMetrics.IntervalInMin),)
+        g = GaugeMetricFamily('hpilo_power_average', 'Average power consumption in Watts.',
+                              labels=self.label_names + ('IntervalInMin',), unit='watts')
+        g.add_metric(label_values, power.PowerMetrics.AverageConsumedWatts)
+        yield g
+        g = GaugeMetricFamily('hpilo_power_min', 'Min power consumption in Watts.',
+                              labels=self.label_names + ('IntervalInMin',), unit='watts')
+        g.add_metric(label_values, power.PowerMetrics.MinConsumedWatts)
+        yield g
+        g = GaugeMetricFamily('hpilo_power_max', 'Max power consumption in Watts.',
+                              labels=self.label_names + ('IntervalInMin',), unit='watts')
+        g.add_metric(label_values, power.PowerMetrics.MaxConsumedWatts)
+        yield g
 
 
 # Create Flask app
@@ -101,9 +112,8 @@ if __name__ == '__main__':
     port = int(os.getenv('ILO_PORT', 443))
     user = os.getenv('ILO_USER', 'admin')
     password = os.getenv('ILO_PASSWORD', 'password')
-    protocol = os.getenv('ILO_PROTOCOL', 'HTTP')
 
-    collector = ILOCollector(hostname, port, user, password, protocol)
+    collector = ILOCollector(hostname, port, user, password)
     REGISTRY.register(collector)
 
     exporter_port = int(os.getenv('LISTEN_PORT', 9116))
